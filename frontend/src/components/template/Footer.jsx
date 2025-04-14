0b35887fa5022728978eb5686c53f44a72758d2c
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-14 10:25:48 -0300

    refactor: enhance Navigation component

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,9 @@ import React from "react";
 
 export default (props) => 
 <footer className="footer">
-    rodapé
+    <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Adryz
+            <span className="text-danger">_.</span>z
+        </strong>
+    </span>
     </footer>;
--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import './Nav.css'
 
-export default props =>
+export default props => 
     <aside className="menu-area">
-        Nav
+        <nav className="menu">
+            {/* Refatorar em casa! */}
+            <a href="#/">
+                <i className="fa fa-home"> Inicio</i>
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"> Friends</i>
+            </a>
+        </nav>
     </aside>
